9217bc2bd35c4d81b557c4c499d1bc3eb819c99d
Eli <elicoh87@gmail.com>
2026-04-09 03:21:04 +0300

    resolve main jsx conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DjBroitiSite from "./App";
+import DjBroitiSite from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DjBroitiSite />
   </React.StrictMode>
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import DjBroitiSite from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <DjBroitiSite />
-  </React.StrictMode>
->>>>>>> 8415e95b0f0494d21d360e78a36f0015e3a3897f
 );
